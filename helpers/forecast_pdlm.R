010af91604fa8dd474cb8bf24c5520ebd430bfc0
John Zito <johnczito@gmail.com>
2024-05-27 19:22:05 -0500

    helpers for generating forecasts in the general spherical case

--- helpers/forecast_pdlm.R
+++ helpers/forecast_pdlm.R
@@ -15,6 +15,22 @@ forecast_angle_basic_pdlm_gibbs <- function(S_draws, Ft, Sig, G, W){
   return(forecasts)
 }
 
+forecast_basic_pdlm_gibbs <- function(S_draws, Ft, Sig, G, W){
+  
+  p = nrow(S_draws)
+  ndraw = ncol(S_draws)
+  forecasts = matrix(0, p, ndraw)
+  
+  for(j in 1:ndraw){
+    old_s = S_draws[, j]
+    new_s = G %*% old_s + mvrnorm(n = 1, mu = numeric(p), W)
+    new_y = Ft %*% new_s + mvrnorm(n = 1, mu = numeric(p), Sig)
+    forecasts[, j] = new_y / sqrt(sum(new_y^2))
+  }
+  
+  return(forecasts)
+}
+
 forecast_angle_pdlm_3_gibbs <- function(S_draws, Ft, Sig_draws, G, W){
   
   p = nrow(S_draws)
@@ -66,6 +82,25 @@ forecast_angle_pdlm_gibbs <- function(S_draws, Ft, V_draws, G_draws, W_draws){
     new_y = Ft %*% new_s + mvrnorm(n = 1, mu = numeric(p), V) # WHY IS THIS p?
     angle = unitcircle2radians(t(new_y))
     forecasts[j] = angle
+  }
+  
+  return(forecasts)
+}
+
+forecast_pdlm_gibbs <- function(S_draws, Ft, V_draws, G_draws, W_draws){
+  
+  p = nrow(S_draws)
+  ndraw = ncol(S_draws)
+  forecasts = matrix(0, p, ndraw)
+  
+  for(j in 1:ndraw){
+    old_s = S_draws[ , j]
+    G = G_draws[, , j]
+    W = W_draws[, , j]
+    V = V_draws[, , j]
+    new_s = G %*% old_s + mvrnorm(n = 1, mu = numeric(p), W)
+    new_y = Ft %*% new_s + mvrnorm(n = 1, mu = numeric(p), V) # WHY IS THIS p?
+    forecasts[, j] = new_y / sqrt(sum(new_y^2))
   }
   
   return(forecasts)
