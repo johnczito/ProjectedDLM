49c3e06478aa7776a2f4211a2aa198f3b2059772
John Zito <johnczito@gmail.com>
2025-07-07 10:13:49 -0400

    matlab script for ohare

---
+++ from_matlab_kurz_results_ohare.m
@@ -0,0 +1,80 @@
+% https://github.com/libDirectional/libDirectional 
+
+clear;
+rng(8675309);
+
+% =========================================================================
+% load data
+% =========================================================================
+
+data_degrees = readmatrix('datasets/black_mountain_wind_direction.csv');
+data_radians = data_degrees * pi / 180;
+data_unitvec = [cos(data_radians) sin(data_radians)];
+
+% =========================================================================
+% store dimensions
+% =========================================================================
+
+T = length(data_radians);
+n = size(data_unitvec, 2);
+M = 2500;
+
+% =========================================================================
+% estimate static parameters on a pre-sample
+% =========================================================================
+
+T0 = T;
+
+Rbar = norm(mean(data_unitvec(1:T0, :), 1));
+
+kappa = Rbar * (n - Rbar^2) / (1 - Rbar^2);
+
+% =========================================================================
+% pre-allocate storage
+% =========================================================================
+
+vmf_forecast_draws = zeros(T, M);
+
+% =========================================================================
+% initialize filters
+% =========================================================================
+
+vmf_filter = VMFFilter();
+vmf_filter.setState(VMFDistribution([1; 0], 1));
+
+for t = 1:T
+
+  % =======================================================================
+  % compute prior predictive distribution of state
+  % =======================================================================
+
+  vmf_filter.predictIdentity(VMFDistribution([0; 1], 1));
+
+  vmf_prior = vmf_filter.getEstimate();
+
+  % =======================================================================
+  % simulate one-step-ahead predictive distribution
+  % =======================================================================
+
+  for m = 1:M
+
+    vmf_s_draw = sample(vmf_prior, 1);
+    vmf_y_draw = sample(VMFDistribution(vmf_s_draw, kappa), 1);
+
+    vmf_forecast_draws(t, m) = mod(atan2(vmf_y_draw(2), vmf_y_draw(1)), 2 * pi);
+
+  end
+
+  % =======================================================================
+  % compute filtering distribution
+  % =======================================================================
+
+  vmf_filter.updateIdentity(VMFDistribution([0; 1], kappa), data_unitvec(t, :)');
+
+end
+
+% =========================================================================
+% preallocate storage
+% =========================================================================
+
+writematrix(vmf_forecast_draws, 'from_matlab_vmf_black_mountain_forecasts.csv')
